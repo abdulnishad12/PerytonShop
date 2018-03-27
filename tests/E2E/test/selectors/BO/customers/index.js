71fc6fd39546a9116389d87d7beb1f9247104f15
abdulnishad12 <abdulnishad12@gmail.com>
2018-03-28 00:00:55 +0530

    Revert "Rebase"
    This reverts commit 95fa657840054775cac4764379ed669f20469b38.

--- tests/E2E/test/selectors/BO/customers/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = Object.assign(
-    {
-        BO: {
-            success_panel: '//*[@id="content"]/div[@class="bootstrap"]/div[contains(@class, "success")]',
-        }
-    },
-    require('./addresses'),
-    require('./customer')
-);
